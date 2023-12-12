ab85a11544a200233b45739beff0e7d15f69d5b6
KAZUYA AKIMOTO <umecchi1098@gmail.com>
2023-12-12 07:58:17 +0000

    faviconとAI_NAMEの調整

--- src/features/chat/chat-ui/chat-empty-state/start-new-chat.tsx
+++ src/features/chat/chat-ui/chat-empty-state/start-new-chat.tsx
@@ -18,11 +18,10 @@ export const StartNewChat: FC<Prop> = (props) => {
         </Typography>
         <div className="flex flex-col gap-2">
           <p className="">
-            Welcome to {AI_NAME}. You should interact in a friendly manner with
-            the AI assistant and refrain from participating in any harmful
-            activities.
+            こんにちは！{AI_NAME}です。AIアシスタントとは友好的にやりとりをし、
+            公序良俗に反するような言葉遣いや質問をしないでください。
           </p>
-          <p>You can start a new chat with me by clicking the button below.</p>
+          <p>以下のボタンをクリックすることで、私との新しいチャットを開始できます。</p>
         </div>
         <div className="-mx-5 -mb-5 p-5 flex flex-col border-t bg-muted">
           <NewChat />
