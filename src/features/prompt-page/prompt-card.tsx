ec8bc9e653857721f332954c4b189bb4eab8ee7d
Kazuya Akimoto <umecchi1098@gmail.com>
2024-07-01 09:01:56 +0000

    feat: Add Lucide icons to PromptCard
    The PromptCard component now displays either a GlobeIcon or UserIcon based on the isPublished property of the prompt. This change improves the visual representation of published and unpublished prompts.

--- src/features/prompt-page/prompt-card.tsx
+++ src/features/prompt-page/prompt-card.tsx
@@ -2,6 +2,7 @@ import { FC } from "react";
 import { Card, CardContent, CardHeader, CardTitle } from "../ui/card";
 import { PromptModel } from "./models";
 import { PromptCardContextMenu } from "./prompt-card-context-menu";
+import { GlobeIcon, UserIcon } from "lucide-react";
 
 interface Props {
   prompt: PromptModel;
@@ -12,13 +13,19 @@ export const PromptCard: FC<Props> = (props) => {
   const { prompt } = props;
   return (
     <Card key={prompt.id} className="flex flex-col">
-      <CardHeader className="flex flex-row">
-        <CardTitle className="flex-1">{prompt.name}</CardTitle>
+      <CardHeader className="flex flex-row items-center">
+        {prompt.isPublished
+          ? <GlobeIcon size={20} className="mr-2" />
+          : <UserIcon size={20} className="mr-2" />
+        }
+        <CardTitle className="flex-1 flex item-center">{prompt.name}</CardTitle>
+
         {props.showContextMenu && (
           <div>
             <PromptCardContextMenu prompt={prompt} />
           </div>
         )}
+      
       </CardHeader>
       <CardContent className="text-muted-foreground flex-1">
         {prompt.description.length > 100
